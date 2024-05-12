f66a6dc5137b8e5fa7630773559bf46b3d6c72cf
starlight1111111 <seanand0123@gmail.com>
2024-05-12 13:23:08 +0800

    Revert "Revert "Update 殭屍攻擊""
    This reverts commit c707b44393b38f0982e3b8c035a7cfef50f45b6d.

---
+++ Assets/scripts/HpSystem.cs
@@ -0,0 +1,23 @@
+using UnityEditor.MPE;
+using UnityEngine;
+
+public class HpSystem : MonoBehaviour
+{
+    [SerializeField, Header("¦å¶q") ,Range(0, 500)]
+    private float hp;
+
+    protected void Damage(float damage)
+    {
+        hp -= damage;
+
+        if (hp <= 0) Dead();
+
+        print($"<color=#f69>{gameObject.name} ¦å¶q: {hp}</Color>");
+    }
+
+    protected virtual void Dead()
+    {
+
+       print($"<color=#f33>{gameObject.name} ¦º¤`: {hp}</Color>");
+    }
+}
